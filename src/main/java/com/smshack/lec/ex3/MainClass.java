74bb110ac3a50aa14e901b9a4b17c8f16535fd32
smshack <5432tat@naver.com>
2020-09-16 05:07:24 +0900

    3. DI( Dependency Injection) * 스프링을 이용한 객체 생성과 조립 -xml 파일 이용 객체 생성 변수 설정 필드 설정
    객체를 만들고 그 객체의 이름 매칭될 클래스명
    그 객체를 이름으로 인식해서
    값을 설정
    
    
    
    * 스프링 설정 파일의 이해
    외부에서 만든 값이 들어간다
    빈을 만들때 setter가 있어야 동작이 된다

---
+++ src/main/java/com/smshack/lec/ex3/MainClass.java
@@ -0,0 +1,18 @@
+package com.smshack.lec.ex3;
+
+public class MainClass {
+public static void main(String[] args) {
+		
+		MyCalculator myCalculator = new MyCalculator();
+		myCalculator.setCalculator(new Calculator());
+		
+		myCalculator.setFirstNum(10);
+		myCalculator.setSecondNum(2);
+		
+		myCalculator.add();
+		myCalculator.sub();
+		myCalculator.mul();
+		myCalculator.div();
+		
+	}
+}
