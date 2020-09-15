42e24ecff18ca7322f7a17a2dd545f9c357bde17
smshack <5432tat@naver.com>
2020-09-16 02:00:04 +0900

    DI(Dependency Injection)과 IOC 컨테이너 B객체와  C객체는 A 객체에 의존한다
    A 객체	->DI
    
    B C D E X Y Z 객체 	->IOC컨테이너 인터페이스를 통한 부품화
    
    스프링이란
    부품을 생성하고 조립하는 라이브러리 집합체

--- src/main/java/com/smshack/lec/ex1/Calculation.java
+++ src/main/java/com/smshack/lec/ex1/Calculation.java
@@ -1,5 +1,39 @@
 package com.smshack.lec.ex1;
 
 public class Calculation {
-
+	private int firstNum;
+	private int secondNum;
+	
+	public int getFirstNum() {
+		return firstNum;
+	}
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+	public int getSecondNum() {
+		return secondNum;
+	}
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+	public void add() {
+		// TODO Auto-generated method stub
+		System.out.println("add()");
+		System.out.println(firstNum+" + "+secondNum+" = "+(firstNum+secondNum) );
+	}
+	public void sub() {
+		// TODO Auto-generated method stub
+		System.out.println("sub()");
+		System.out.println(firstNum+" - "+secondNum+" = "+(firstNum-secondNum) );
+	}
+	public void mult() {
+		// TODO Auto-generated method stub
+		System.out.println("mult()");
+		System.out.println(firstNum+" * "+secondNum+" = "+(firstNum*secondNum) );
+	}
+	public void div() {
+		// TODO Auto-generated method stub
+		System.out.println("div()");
+		System.out.println(firstNum+" / "+secondNum+" = "+(firstNum/secondNum) );
+	}
 }
--- src/main/java/com/smshack/lec/ex1/MainClass.java
+++ src/main/java/com/smshack/lec/ex1/MainClass.java
@@ -1,5 +1,14 @@
 package com.smshack.lec.ex1;
 
 public class MainClass {
-
+	public static void main(String[] args) {
+		Calculation cal = new Calculation();
+		cal.setFirstNum(10);
+		cal.setSecondNum(2);
+		
+		cal.add();
+		cal.sub();
+		cal.mult();
+		cal.div();
+	}
 }
